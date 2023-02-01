659252db6277baa193fb1d6d16f327cb9f232cf0
azarnuzy <azarnuzy@gmail.com>
2023-02-02 03:44:51 +0700

    setup folders and files

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Portofolio</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
