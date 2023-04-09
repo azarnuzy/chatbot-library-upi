1a6741595619e9c21da0facbff7e2b211093e405
azarnuzy <azarnuzy@gmail.com>
2023-04-09 14:06:05 +0700

    add option input and auto scroll to bottom chat container

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='fixed bottom-0 z-10 w-full py-3 text-white  bg-dark-gray px-8 mx-auto text-center'>
+    <div className='lg:fixed lg:bottom-0 z-10 w-full py-3 text-white  bg-dark-gray px-8 mx-auto text-center'>
       <p className='text-lg font-semibold'>
         Copyright 2023 <br /> Universitas Pendidikan Indonesia
       </p>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Bot from '../Assets/Icons/Bx_bot'
 import Vector from '../Assets/Icons/Vector'
 import Send from '../Assets/Icons/send'
 import SEOHead from '../Components/SEO/SEOHead'
 import homeImg from '../Assets/Images/home.png'
+import axios from 'axios'
 
 function Home() {
   const firstText =
@@ -13,20 +14,48 @@ function Home() {
     { speaker: 'bot', message: firstText },
   ])
 
+  const containerRef = useRef(0)
   const [inputUser, setInputUser] = useState([])
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const postRes = async (input) => {
+    // const res = await axios.post(
+    //   'http://perpustakaan.upi.edu:5000/predict',
+    //   {
+    //     input: inputUser,
+    //   },
+    //   {
+    //     headers: {
+    //       'Content-Type': 'multipart/form-data',
+    //     },
+    //   }
+    // )
+
     const newChatLog = [
       ...chatLog,
-      { speaker: 'user', message: inputUser },
+      { speaker: 'user', message: input },
       {
         speaker: 'bot',
         message: 'Saya tidak mengerti maksud anda. Coba tanyakan ulang!',
+        option: [
+          'Kapan perpustakaan buka?',
+          'Bagaimana cara meminjam buku?',
+          'Saya ingin memperpanjang peminjaman buku',
+        ],
       },
     ]
     setChatLog(newChatLog)
     setInputUser('')
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await postRes(inputUser)
+    containerRef.current.scrollTo(0, containerRef.current.scrollHeight)
+  }
+  const handleClick = async (item) => {
+    setInputUser(inputUser)
+    await postRes(item)
+    containerRef.current.scrollTo(0, containerRef.current.scrollHeight)
   }
 
   return (
@@ -38,8 +67,8 @@ function Home() {
         image={homeImg}
         url='https://v1-dev-chatbot-upi.netlify.app/'
       />
-      <div className='flex gap-8 justify-around'>
-        <div className='w-1/2 mt-[10vh] flex justify-end'>
+      <div className='flex flex-col justify-center sm:flex-row gap-8 sm:justify-around'>
+        <div className='w-full sm:w-1/2 mt-5 sm:mt-10 lg:mt-[3vh]  flex justify-center sm:justify-end'>
           <div className='shadow-xl border-[1px] border-solid border-slate-200 rounded-t-xl max-w-2xl min-w-xl lg:w-[80%] w-[90%] relative'>
             <div className='bg-light-red rounded-t-xl flex items-center gap-3 px-8'>
               <div className='rounded-full p-2 flex items-center justify-center w-[40px] h-[38px] bg-light-gray my-3'>
@@ -53,13 +82,16 @@ function Home() {
                 <p className='font-medium'>Chatbot Support</p>
               </div>
             </div>
-            <div className='h-[50vh] pt-4 overflow-x-hidden overflow-y-scroll  p-2'>
+            <div
+              ref={containerRef}
+              className='h-[50vh] pt-4 overflow-x-hidden overflow-y-scroll  p-2'
+            >
               {chatLog.length === 1 && (
                 <div className='ml-3 w-full flex gap-3 mb-5'>
                   <div className='rounded-full p-2 flex items-center justify-center w-[40px] h-[38px] bg-light-gray'>
                     <Bot />
                   </div>
-                  <div className='w-full bg-light-silver rounded-xl p-2 shadow-md mr-14'>
+                  <div className='w-fit bg-light-silver rounded-xl p-2 shadow-md mr-14'>
                     {chatLog.length === 1 && firstText}
                   </div>
                 </div>
@@ -68,21 +100,32 @@ function Home() {
                 return (
                   <>
                     {chatLog.length !== 1 && item.speaker === 'bot' && (
-                      <div className='ml-3 w-full flex gap-3 mb-5'>
-                        <div className='rounded-full p-2 flex items-center justify-center w-[40px] h-[38px] bg-light-gray'>
-                          <Bot />
+                      <>
+                        <div className='ml-3 w-full flex gap-3 mb-5'>
+                          <div className='rounded-full p-2 flex items-center justify-center w-[40px] h-[38px] bg-light-gray'>
+                            <Bot />
+                          </div>
+                          <div className='w-fit bg-light-silver rounded-xl p-2 shadow-md mr-14'>
+                            {item.message}
+                          </div>
                         </div>
-                        <div className='w-full bg-light-silver rounded-xl p-2 shadow-md mr-14'>
-                          {chatLog.length !== 1 &&
-                            item.speaker === 'bot' &&
-                            item.message}
-                        </div>
-                      </div>
+                        {item?.option?.length > 0 &&
+                          item.option.map((suggest) => (
+                            <div className='ml-3 w-full flex gap-3 mb-2'>
+                              <button
+                                onClick={() => handleClick(suggest)}
+                                className='ml-[52px] shadow w-fit border-1 border border-solid border-light-silver  rounded-xl p-2 mr-14'
+                              >
+                                {suggest}
+                              </button>
+                            </div>
+                          ))}
+                      </>
                     )}
                     {item.speaker === 'user' && (
                       <div className='w-full flex justify-end gap-3 mb-5'>
                         <div className='w-fit  bg-light-silver rounded-xl p-2 shadow-md ml-14'>
-                          {item.speaker === 'user' && item.message}
+                          {item.message}
                         </div>
                         <div className='rounded-full p-2 flex items-center justify-center w-[40px] h-[38px] bg-light-gray'>
                           <Vector />
@@ -116,7 +159,7 @@ function Home() {
             </div>
           </div>
         </div>
-        <div className='w-1/2 max-w-xl lg:pr-10 font-medium text-md flex flex-col justify-center mt-[10vh]'>
+        <div className='w-full sm:w-1/2 max-w-xl lg:pr-10 font-medium text-md flex flex-col justify-center px-3 mb-24 sm:mb-0  sm:mt-[10vh]'>
           <h3 className='text-lg font-bold'>
             Layanan Chatbot Perpustakaan Universitas Pendidikan Indonesia
           </h3>
